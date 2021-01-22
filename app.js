e7652ddb2644d95b54846e680a23092cf48d373a
Uriel <urielbenitez96@hotmail.com>
2021-01-22 09:38:19 -0600

    prueba implementacion cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -16,7 +17,7 @@ const sessionMiddleware = session({
   resave: false,
 });
 const app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
